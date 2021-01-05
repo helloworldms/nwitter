43c694e385413b71588043c951224c1993d1ebae
helloworldms <72796025+helloworldms@users.noreply.github.com>
2021-01-05 23:54:43 +0900

    firebase

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,36 +1,29 @@
-import React, { useState } from "react";
-import {
-  HashRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Profile from "routes/Profile";
-import Navigation from "./Navigation";
+import Navigation from "components/Navigation";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation></Navigation>}
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
-              <Home />
+              <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile />
             </Route>
-            <Redirect from="*" to="/" />
           </>
         ) : (
           <>
             <Route exact path="/">
               <Auth />
             </Route>
-            <Redirect from="*" to="/" />
           </>
         )}
       </Switch>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,68 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
-const Home = (userObj) => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-  const getNweets = async () => {
-    const dbNweets = await dbService.collection("nweets").get();
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setNweets((prev) => [nweetObject, ...prev]);
-      //새로 작성한 트윗과 그 이전의 것
-      //set이 붙을때 함수전달 가능 , 만약 함수가 전달되면 리액트는 이전 값에 접근할 수 있게 함
-    });
-  };
-
-  //컴포넌트가 mount될 때, 우리는 getNweets 실행
-  //getNweets는 dbService 불러옴
-  //collection. nweets를 get써서 가져옴
-
   useEffect(() => {
-    getNweets();
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
   }, []);
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
     await dbService.collection("nweets").add({
       text: nweet,
-      createAt: Date.now(),
-      createId: userObj.uid, // 작성자를 알 수 있음
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
-    setNweets("");
+    setNweet("");
   };
-
-  const onChange = (e) => {
+  const onChange = (event) => {
     const {
       target: { value },
-    } = e;
+    } = event;
     setNweet(value);
   };
-
-  console.log(nweets);
   return (
-    <>
+    <div>
       <form onSubmit={onSubmit}>
         <input
-          type="text"
-          placeholder="input your insprit"
-          maxLength={120}
           value={nweet}
           onChange={onChange}
-        ></input>
-        <input type="submit" value="Nweet"></input>
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
       </form>
-      <div key={nweet.id}>
+      <div>
         {nweets.map((nweet) => (
-          <div>
+          <div key={nweet.id}>
             <h4>{nweet.text}</h4>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 export default Home;
